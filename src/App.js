8074d59f9b92ace289f132807ed31115bfa1481e
bigbit99 <108947985+bigbit99@users.noreply.github.com>
2022-08-14 20:31:01 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ import Write from "./pages/Write";
 import Update from "./pages/Update";
 import Header from "./components/Header";
 import Layout from "./components/Layout";
+import Login from "./pages/Login";
 
 const App = () => {
-  console.log(RESP);
-  console.log(RESP.AUTH_POSTS[0].data.imgUrl);
+  // console.log(RESP);
+  // console.log(RESP.AUTH_POSTS[0].data.imgUrl);
+
   // const [state, setState] = useState("스태이트")
+
   return (
     <BrowserRouter>
       <Header />
@@ -21,6 +24,7 @@ const App = () => {
             <Route path="/" element={<Main />} />
             <Route path="/detail" element={<Detail />} />
             <Route path="/signin" element={<SignIn />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/write" element={<Write />} />
             <Route path="/update" element={<Update />} />
           </Route>
