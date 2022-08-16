f6481493738f1ca4564e4b1421e79c0e8d14bb28
bigbit99 <108947985+bigbit99@users.noreply.github.com>
2022-08-16 13:14:01 +0900

    no message

--- src/redux/modules/instance.js
+++ src/redux/modules/instance.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: "http://3.39.10.121/api/",
-  });
+});
 
-const token = localStorage.getItem("token");
+// const token = localStorage.getItem("token");
 
 
 export default instance;
