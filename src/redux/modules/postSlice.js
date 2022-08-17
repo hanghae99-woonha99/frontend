3335dc82a32dc772b907540c8b3753b019953185
bigbit99 <108947985+bigbit99@users.noreply.github.com>
2022-08-17 14:59:15 +0900

    post 슬라이스 수정

--- src/redux/modules/postSlice.js
+++ src/redux/modules/postSlice.js
@@ -1,63 +1,56 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import instance from "../../shared/Request";
 
+const headers = {
+  'Content-Type': 'multipart/form-data',
+  'Authorization': sessionStorage.getItem("token")
+}
+
+export const postPostThunk = createAsyncThunk(
+  "POST_POST",
+  async (payload, thunkAPI) => {
+    console.log("들어와라");
+    try { 
+      console.log("들어와라222");
+      console.log(sessionStorage.getItem("token"));
+      const data = await instance.post("auth/posts", payload, { 
+        headers: headers
+        // headers: {
+        //   "Content-Type": "multipart/form-data",
+        //   withCredentials: true,
+        //   Authorization: sessionStorage.getItem("token")
+        // }
+      });
+      return thunkAPI.fulfillWithValue(data.data);
+    } catch (e) {
+      return console.log(e);
+    }
+  }
+);
+
 
 const initialState = {
-    post : {
-        postId: 0,
-        title: "",
-        descript: "",
-        imgUrl: "",
-        author: "",
-        postLikeCnt: 0,
-        commentCnt: 0,
-    },
-    error: null,
+  post: [],
+  error: null,
 };
 
-export const addPostThunk = createAsyncThunk(
-    "POST_POST",
-    async (payload, thunkAPI) => {
-      try {
-        const { data } = await instance.post("auth/posts", payload, {
-          "Content-Type": "multipart/form-data",
-          withCredentials: true,
-        });
-        return window.location.replace("/");
-      } catch (e) {
-        return console.log(e);
-      }
-    }
-  );
-
-  export const postSlice = createSlice({
-    name: "posts",
-    initialState,
-    reducers: {},
-    extraReducers: {
-      [addPostThunk.fulfilled]: (state, action) => {
-        state.posts = [...state.posts, action.payload];
-      },
-      [addPostThunk.rejected]: (state, action) => {
-        state.error = action.payload;
-      },
-      [addPostThunk.pending]: () => {},
+export const postSlice = createSlice({
+  name: "post",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [postPostThunk.fulfilled]: (state, action) => {
+      state.post = [...state.post, action.payload];
     },
-  });
+    [postPostThunk.rejected]: (state, action) => {
+      state.error = action.payload;
+    },
+    [postPostThunk.pending]: (state, action) => {
+      
+    },
 
-// const postSlice = createSlice({
-//     name: "post",
-//     initialState,
-//     reducers: {
-//         //리듀서 안에서 만든 함수자체가 리듀서의 로직이자, 액션크리에이터!
-//         addPost: (state, action) => {
-//             console.log(action)
-//             state.name = "한빛"
-//         }
-//     }
-// })
+  },
+});
 
-// 액션크리에이터를 컴포넌트에서 사용하기 위해 export 후 컴포넌트에 가서 useselector로 불러옴 
-export const { addPost } = postSlice.actions;
-
+// export const {} = postsSlice.actions;
 export default postSlice.reducer;
