38a2baf9af14890f170c928ade61c88f8a054c8c
bigbit99 <108947985+bigbit99@users.noreply.github.com>
2022-08-18 01:30:48 +0900

    no message

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useParams, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
